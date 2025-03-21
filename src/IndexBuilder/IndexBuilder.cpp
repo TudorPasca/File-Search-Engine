4a6d27b29411f64b7002c6f47466d67d8f6f5c9f
Tudor Pasca <tudorpasca11@gmail.com>
2025-03-21 17:47:54 +0200

    added filename to IndexBuilder insert query

--- src/IndexBuilder/IndexBuilder.cpp
+++ src/IndexBuilder/IndexBuilder.cpp
@@ -9,13 +9,15 @@ void IndexBuilder::indexFiles(const std::filesystem::path &path) const {
         pqxx::connection conn(CONNECTION_STRING);
         pqxx::work txn(conn);
         for (const auto &file: files) {
-            std::string query = R"(INSERT INTO public.file ("path", "content", is_folder)
+            std::string query = R"(INSERT INTO public.file (filename, path, content, is_folder)
                        VALUES ( )"
+                                + txn.quote(file.getName()) + ", "
                                 + txn.quote(file.getAbsolutePath()) + ", "
                                 + txn.quote(file.getContents()) + ", "
                                 + txn.quote(file.isFolder()) + R"()
-                       ON CONFLICT ("path") DO UPDATE SET
-                       "content" = EXCLUDED."content",
+                       ON CONFLICT (path) DO UPDATE SET
+                       filename = EXCLUDED.filename,
+                       content = EXCLUDED.content,
                        is_folder = EXCLUDED.is_folder)";
             txn.exec(query);
         }
