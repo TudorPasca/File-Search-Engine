ee7946703a8fd34fe2efc9c6b4ea0f9015362cdc
Tudor Pasca <tudorpasca11@gmail.com>
2025-03-22 15:19:32 +0200

    IndexBuilder now writes to log file upon indexing

--- include/IndexBuilder/IndexBuilder.h
+++ include/IndexBuilder/IndexBuilder.h
@@ -2,17 +2,28 @@
 
 #include <utility>
 #include <filesystem>
+#include <fstream>
 #include <pqxx/pqxx>
+#include <iostream>
 #include "FileScraper.h"
 
 class IndexBuilder {
 public:
     IndexBuilder() = default;
-    explicit IndexBuilder(std::shared_ptr<IFileScraper> scraper, const std::string &connectionString):
-        scraper(std::move(scraper)), CONNECTION_STRING(connectionString) {};
 
-    void indexFiles(const std::filesystem::path& path) const;
+    explicit IndexBuilder(std::shared_ptr<IFileScraper> scraper, const std::string &connectionString) :
+            scraper(std::move(scraper)), CONNECTION_STRING(connectionString) {
+        logFile.open("index_log.txt", std::ios::app);
+        if (!logFile.is_open()) {
+            std::cerr << "Error opening progress file!" << std::endl;
+        }
+    };
+
+    void indexFiles(const std::filesystem::path &path);
 private:
-    std::shared_ptr<IFileScraper> scraper;
     const std::string CONNECTION_STRING;
+    std::shared_ptr<IFileScraper> scraper;
+    std::ofstream logFile;
+
+    void writeToLog(const std::filesystem::path &path, std::vector<FileDTO> files);
 };
--- src/IndexBuilder/IndexBuilder.cpp
+++ src/IndexBuilder/IndexBuilder.cpp
@@ -1,10 +1,14 @@
 #include "../../include/IndexBuilder/IndexBuilder.h"
-#include <iostream>
+
+#include <ostream>
+#include <iomanip>
+#include <chrono>
 
 namespace fs = std::filesystem;
 
-void IndexBuilder::indexFiles(const std::filesystem::path &path) const {
+void IndexBuilder::indexFiles(const std::filesystem::path &path) {
     std::vector<FileDTO> files = scraper->getFilesRecursively(path);
+    writeToLog(path, files);
     try {
         pqxx::connection conn(CONNECTION_STRING);
         pqxx::work txn(conn);
@@ -25,4 +29,23 @@ void IndexBuilder::indexFiles(const std::filesystem::path &path) const {
     } catch (const std::exception &e) {
         std::cerr << "Error during file indexing: " << e.what() << std::endl;
     }
+}
+
+void IndexBuilder::writeToLog(const std::filesystem::path &path, std::vector<FileDTO> files) {
+    if (!logFile.is_open()) {
+        std::cerr << "Log file is not open!" << std::endl;
+        return;
+    }
+
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    std::stringstream time_ss;
+    time_ss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+
+    logFile << "[" << time_ss.str() << "] Indexing path: " << path.string() << std::endl;
+    for (const auto &file: files) {
+        logFile << "  - " << file.getAbsolutePath() << (file.isFolder() ? " (Folder)" : " (File)") << std::endl;
+    }
+    logFile << "----------------------------------------" << std::endl;
+    logFile.flush();
 }
