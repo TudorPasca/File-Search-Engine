c10fada68f495d48646784476c555679d35c86ee
Tudor Pasca <tudorpasca11@gmail.com>
2025-04-08 18:12:28 +0300

    Fix FileDTO naming inconsistency

--- include/DTO/FileDTO.h
+++ include/DTO/FileDTO.h
@@ -6,18 +6,19 @@
 
 class FileDTO {
 public:
-    FileDTO(std::string  name, std::string  absolutePath, bool folder, std::string contents, std::string extension, uint64_t size_bytes);
+    FileDTO(std::string  name, std::string  absolutePath, bool folder, std::string contents, std::string extension, uint64_t sizeBytes);
     explicit FileDTO(const std::filesystem::directory_entry &entry);
     const std::string& getName() const;
     const std::string& getAbsolutePath() const;
     const bool isFolder() const;
     const std::string& getContents() const;
-    std::string getExtension() const;
+    const std::string getExtension() const;
+    const uint64_t getSizeBytes() const;
 private:
     std::string name;
     std::string absolutePath;
     bool folder;
     std::string contents;
     std::string extension;
-    uint64_t size_bytes;
+    uint64_t sizeBytes;
 };
--- src/DTO/FileDTO.cpp
+++ src/DTO/FileDTO.cpp
@@ -4,9 +4,9 @@
 #include <sstream>
 
 FileDTO::FileDTO(std::string name, std::string absolutePath, bool folder, std::string contents, std::string extension,
-                 uint64_t size_bytes) :
+                 uint64_t sizeBytes) :
         name(std::move(name)), absolutePath(std::move(absolutePath)), folder(folder), contents(std::move(contents)),
-        extension(std::move(extension)), size_bytes(size_bytes) {}
+        extension(std::move(extension)), sizeBytes(sizeBytes) {}
 
 FileDTO::FileDTO(const std::filesystem::directory_entry &entry) {
     name = entry.path().filename().string();
@@ -24,7 +24,7 @@ FileDTO::FileDTO(const std::filesystem::directory_entry &entry) {
     if (!folder && extensionDot != std::string::npos && extensionDot != 0 && extensionDot != absolutePath.size() - 1) {
         extension = absolutePath.substr(extensionDot + 1);
     }
-    size_bytes = std::filesystem::file_size(entry);
+    sizeBytes = std::filesystem::file_size(entry);
 }
 
 const std::string &FileDTO::getName() const { return name; }
@@ -35,12 +35,6 @@ const bool FileDTO::isFolder() const { return folder; }
 
 const std::string &FileDTO::getContents() const { return contents; }
 
-std::string FileDTO::getExtension() const {
-    std::string::size_type lastDot = absolutePath.find_last_of('.');
-    std::string::size_type lastSlash = absolutePath.find_last_of("/\\");
-    if (lastDot != std::string::npos
-        && (lastSlash == std::string::npos || lastDot > lastSlash)) {
-        return absolutePath.substr(lastDot + 1);
-    }
-    return "";
-}
+const std::string FileDTO::getExtension() const { return extension; }
+
+const uint64_t FileDTO::getSizeBytes() const { return sizeBytes; }
